2dfd934ea80fee55a58e8e16358f5a277aa40815
RAHUL SHARMA <95410264+rahulsharmadev-community@users.noreply.github.com>
2022-05-08 02:10:42 +0530

    add some images

--- lib/src/const/icog_const.dart
+++ lib/src/const/icog_const.dart
@@ -1,89 +1,113 @@
 import 'package:flutter/painting.dart';
 
-/// Obamas Resignation
-/// ![Image](assets/icogObamasResignation.jpg)
+/// ## Obamas Resignation
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogObamasResignation.jpg)
 const icogObamasResignation = [Color(0xff5f0a87), Color(0xffa4508b)];
 
-/// Global Warming
+/// ## Global Warming
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogGlobalWarming.jpg)
 const icogGlobalWarming = [Color(0xff20bf55), Color(0xff01baef)];
 
-/// Successful Immigrant
+/// ## Successful Immigrant
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogSuccessfulImmigrant.jpg)
 const icogSuccessfulImmigrant = [Color(0xff74f2ce), Color(0xff7cffcb)];
 
-/// Casey Broken Camera
+/// ## Casey Broken Camera
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogCaseyBrokenCamera.jpg)
 const icogCaseyBrokenCamera = [Color(0xffa40606), Color(0xffd98324)];
 
-/// Jack Dorseystock
+/// ## Jack Dorseystock
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogJackDorseystock.jpg)
 const icogJackDorseystock = [Color(0xff380036), Color(0xff0cbaba)];
 
-/// HomelessManJoy
+/// ## HomelessManJoy
+///  ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogHomelessManJoy.jpg)
 const icogHomelessManJoy = [Color(0xffe58c8a), Color(0xffeec0c6)];
 
-/// Spotify Free Features
+/// ## Spotify Free Features
+///  ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogSpotifyFreeFeatures.jpg)
 const icogSpotifyFreeFeatures = [Color(0xff80ff72), Color(0xff7ee8fa)];
 
-/// Hodor Sentence
+/// ## Hodor Sentence
+///  ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogHodorSentence.jpg)
 const icogHodorSentence = [Color(0xff7ee8fa), Color(0xffeec0c6)];
 
-/// Mars Conquest
+/// ## Mars Conquest
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogHodorSentence.jpg)
 const icogMarsConquest = [Color(0xffec9f05), Color(0xffff4e00)];
 
-/// Wozniak Broken Heart
+/// ## Wozniak Broken Heart
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogWozniakBrokenHeart.jpg)
 const icogWozniakBrokenHeart = [Color(0xff9fa4c4), Color(0xffb3cdd1)];
 
-/// Housewife Revenue
+/// ## Housewife Revenue
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogHousewifeRevenue.jpg)
 const icogHousewifeRevenue = [Color(0xff9e768f), Color(0xff9fa4c4)];
 
-/// EntrepreneurialLies
+/// ## EntrepreneurialLies
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogEntrepreneurialLies.jpg)
 const icogEntrepreneurialLies = [Color(0xff8693ab), Color(0xffbdd4e7)];
 
-/// Developing Country
+/// ## Developing Country
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogDevelopingCountry.jpg)
 const icogDevelopingCountry = [Color(0xffb279a7), Color(0xffd387ab)];
 
-/// Abstract Truths
+/// ## Abstract Truths
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogAbstractTruths.jpg)
 const icogAbstractTruths = [Color(0xffd387ab), Color(0xffe899dc)];
 
-/// Fraudulent Election
+/// ## Fraudulent Election
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogFraudulentElection.jpg)
 const icogFraudulentElection = [Color(0xff0d324d), Color(0xff7f5a83)];
 
-/// Ocean Blue Some
+/// ## Ocean Blue Some
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogOceanBlue.jpg)
 const icogOceanBlue = [Color(0xff2E3192), Color(0xff1BFFFF)];
 
-/// Sanguine
+/// ## Sanguine
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogSanguine.jpg)
 const icogSanguine = [Color(0xffD4145A), Color(0xffFBB03B)];
 
-/// Luscious Lime
+/// ## Luscious Lime
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogLusciousLime.jpg)
 const icogLusciousLime = [Color(0xff009245), Color(0xffFCEE21)];
 
-/// Purple Lake
+/// ## Purple Lake
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogPurpleLake.jpg)
 const icogPurpleLake = [Color(0xff662D8C), Color(0xffED1E79)];
 
-/// GreenBeach
+/// ## GreenBeach
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogGreenBeach.jpg)
 const icogGreenBeach = [Color(0xff02AABD), Color(0xff00CDAC)];
 
-/// BloodyMary
+/// ## BloodyMary
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogBloodyMary.jpg)
 const icogBloodyMary = [Color(0xffFF512F), Color(0xffDD2476)];
 
-/// SweetMorning
+/// ## SweetMorning
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogSweetMorning.jpg)
 const icogSweetMorning = [Color(0xffFF5F6D), Color(0xffFFC371)];
 
-/// Quepal
+/// ## Quepal
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogQuepal.jpg)
 const icogQuepal = [Color(0xff11998E), Color(0xff38EF7D)];
 
-/// Yosemite
+/// ## Yosemite
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogYosemite.jpg)
 const icogYosemite = [Color(0xffEA8D8D), Color(0xffA890FE)];
 
-/// Exotic
+/// ## Exotic
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogExotic.jpg)
 const icogExotic = [Color(0xffFF61D2), Color(0xffFE9090)];
 
-/// Celestial
+/// ## Celestial
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogCelestial.jpg)
 const icogCelestial = [Color(0xffC33764), Color(0xff1D2671)];
 
-/// Eternal
+/// ## Eternal
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogEternal.jpg)
 const icogEternal = [Color(0xff09203F), Color(0xff537895)];
 
-/// Juicy Peach
-const icogJuicyPeach = [Color(0xffFFECD2), Color(0xffFFECD2)];
-
-/// Plum Plate
+/// ## Plum Plate
+/// ![](https://raw.githubusercontent.com/rahulsharmadev-community/ico/beta/assets/icogPlumPlate.jpg)
 const icogPlumPlate = [Color(0xff764BA2), Color(0xff667EEA)];
